7d749574d19ab6d25910c38f055ffb3539679247
Kieron Kok <2605378@sit.singaporetech.edu.sg>
2026-09-24 18:19:24 +0800

    Added get_valid_input()

--- INF1103-Labs3/modular_auditor.py
+++ INF1103-Labs3/modular_auditor.py
@@ -3,6 +3,23 @@ delivery_processed = 0
 failed_entries = 0
 delivery_tax = 1.5
 
+def get_valid_input():
+    """Prompt once. Return an int (>=0), "quit," or "invalid"."""
+    user_input=input("Enter stock quantity ( or 'quit' to finish): ").strip()
+
+    if user_input() == "quit":
+        return "quit"
+
+    if user_input.lstrip('-').isdigit():
+        quantity = int(user_input)
+        if quantity<0:
+            print("Error: Negative numbers are not allowed.")
+            return "invalid"
+        return quantity
+
+    print("Error: Please enter a valid number")
+    return "invalid"
+
 while True:
     user_input = input("Enter stock quantity ( or 'quit' to finish): ")
 
